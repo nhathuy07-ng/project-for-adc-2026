b2f668e168952e3d7c0dfab37f98f7cff17e1ec7
Huy Nguye <huyndn2007@gmail.com>
2026-09-22 19:36:18 +0700

    feat: implement tts loop

--- tts.py
+++ tts.py
@@ -1,1 +1,25 @@
+from pocket_tts import TTSModel
+import scipy.io.wavfile
 import queue
+import threading
+
+tts_model = TTSModel.load_model()
+voice_state = tts_model.get_state_for_audio_prompt("charles")
+
+job_queue: queue.Queue[tuple[str, str]] = queue.Queue()
+job_results: dict[str, str] = {}
+
+def tts_loop():
+    while True:
+
+        item = job_queue.get()
+
+        voice_line, job_id = item
+
+        audio = tts_model.generate_audio(voice_state, voice_line)
+        scipy.io.wavfile.write(f"tmp/audio_{job_id}.wav", tts_model.sample_rate, audio.numpy())
+        job_results[job_id] = f"tmp/audio_{job_id}.wav"
+        job_queue.task_done()
+
+job_queue.task_done
+tts_thread = threading.Thread(target=tts_loop, daemon=True)
